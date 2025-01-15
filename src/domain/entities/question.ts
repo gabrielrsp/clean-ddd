193cced208c9e01be359daf64e836580f446a28c
Gabriel <gabriel.pires@a3data.com.br>
2025-01-14 22:54:55 -0300

    create value object for slug

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,9 +1,14 @@
+// Value Object é uma forma de abstrair uma regra de negócio específica 
+// de um único campo de uma entidade em uma classe separada
+
 import {  randomUUID } from "node:crypto"
+import { Slug } from "./value-objects/slug"
 
 interface QuestionProps {
   title: string
   content: string 
   authorId: string
+  slug: Slug
 }
 
 export class Question {
@@ -11,11 +16,13 @@ export class Question {
   public content: string
   public id: string
   public authorId: string
+  public slug: Slug
 
   constructor (props: QuestionProps, id?: string ) {
     this.title = props.title
     this.authorId = props.authorId
     this.content = props.content
+    this.slug = props.slug
     this.id = id ?? randomUUID()
   }
 }
