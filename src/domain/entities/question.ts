a69877c6e215c1ead3312d01aa20ec7e7bb5a730
Gabriel <gabriel.pires@a3data.com.br>
2025-01-15 21:48:05 -0300

    mapping properties

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -3,12 +3,16 @@
 
 import { Slug } from "./value-objects/slug"
 import { Entity } from "../../core/entities/entity"
+import { UniqueEntityID } from "../../core/entities/unique-entity-id"
 
 interface QuestionProps {
+  authorId: UniqueEntityID
+  bestAnswerId: UniqueEntityID
   title: string
   content: string 
-  authorId: string
   slug: Slug
+  createdAt: Date
+  updatedAt?: Date
 }
 
 export class Question extends Entity<QuestionProps> {
