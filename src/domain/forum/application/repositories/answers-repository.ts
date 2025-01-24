0496fec6a5eafc3df93bf0170ffe7f8e4e40c6a6
Gabriel <gabriel.pires@a3data.com.br>
2025-01-23 21:40:22 -0300

    refactor folder structure

--- src/domain/forum/application/repositories/answers-repository.ts
+++ src/domain/forum/application/repositories/answers-repository.ts
@@ -1,6 +1,6 @@
 // Repositories Interfaces sets the definition (not the implementation) on persisting data
 
-import { Answer } from '../entities/answer'
+import { Answer } from '../forum/enterprise/entities/answer'
 
 export interface AnswersRepository {
   create(answer: Answer): Promise<void>
