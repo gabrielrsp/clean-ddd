7f50575a9f5d7e8afd64cf27aa9406dedcee6293
Gabriel <gabriel.pires@a3data.com.br>
2025-01-27 22:20:58 -0300

    add fake data for testing

--- test/factories/make-question.ts
+++ test/factories/make-question.ts
@@ -4,15 +4,22 @@ import {
   QuestionProps,
 } from '@/domain/forum/enterprise/entities/question'
 import { Slug } from '@/domain/forum/enterprise/entities/value-objects/slug'
+import { faker } from '@faker-js/faker'
 
-export function makeQuestion(override: Partial<QuestionProps> = {}) {
-  const question = Question.create({
-    title: 'Example question',
-    slug: Slug.create('example-question'),
-    authorId: new UniqueEntityID(),
-    content: 'Example content',
-    ...override,
-  })
+export function makeQuestion(
+  override: Partial<QuestionProps> = {},
+  id?: UniqueEntityID,
+) {
+  const question = Question.create(
+    {
+      title: faker.lorem.sentence(),
+      slug: Slug.create('example-question'),
+      authorId: new UniqueEntityID(),
+      content: faker.lorem.text(),
+      ...override,
+    },
+    id,
+  )
 
   return question
 }
