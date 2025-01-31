f8cba9fa11aac2d1d5b45049cf0197d628d7f1a9
Gabriel <gabriel.pires@a3data.com.br>
2025-01-31 00:27:59 -0300

    add delete answer use case

--- src/domain/forum/application/repositories/answers-repository.ts
+++ src/domain/forum/application/repositories/answers-repository.ts
@@ -1,7 +1,9 @@
 // Repositories Interfaces sets the definition (not the implementation) on persisting data
 
-import { Answer } from '../forum/enterprise/entities/answer'
+import { Answer } from '../../enterprise/entities/answer'
 
 export interface AnswersRepository {
+  findById(id: string): Promise<Answer | null>
   create(answer: Answer): Promise<void>
+  delete(answer: Answer): Promise<void>
 }
