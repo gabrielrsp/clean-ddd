035ae565a72472d0736e7787444388666c827f99
Gabriel <gabriel.pires@a3data.com.br>
2025-02-02 21:11:53 -0300

    implement edit question use case

--- src/domain/forum/application/repositories/questions-repository.ts
+++ src/domain/forum/application/repositories/questions-repository.ts
@@ -7,4 +7,5 @@ export interface QuestionsRepository {
   findBySlug(slug: string): Promise<Question | null>
   create(question: Question): Promise<void>
   delete(question: Question): Promise<void>
+  save(question: Question): Promise<void>
 }
