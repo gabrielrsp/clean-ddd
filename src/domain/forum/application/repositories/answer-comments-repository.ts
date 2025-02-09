fba016d0b28b9dbb439022871f997397226eb730
Gabriel <gabriel.pires@a3data.com.br>
2025-02-09 14:39:09 -0300

    implements fetch answer comments use case

--- src/domain/forum/application/repositories/answer-comments-repository.ts
+++ src/domain/forum/application/repositories/answer-comments-repository.ts
@@ -1,9 +1,14 @@
 // Repositories Interfaces sets the definition (not the implementation) on persisting data
 
+import { PaginationParams } from '@/core/repositories/pagination-params'
 import { AnswerComment } from '../../enterprise/entities/answer-comment'
 
 export interface AnswerCommentsRepository {
   findById(id: string): Promise<AnswerComment | null>
   create(answerComment: AnswerComment): Promise<void>
   delete(answerComment: AnswerComment): Promise<void>
+  findManyByAnswerId(
+    questionId: string,
+    params: PaginationParams,
+  ): Promise<AnswerComment[]>
 }
