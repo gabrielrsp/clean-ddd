439fab0be535bf4814d208d282509d9bcd1e0623
Gabriel <gabriel.pires@a3data.com.br>
2025-02-09 14:28:56 -0300

    implements fetch question comments uuse case

--- src/domain/forum/application/repositories/question-comments-repository.ts
+++ src/domain/forum/application/repositories/question-comments-repository.ts
@@ -1,8 +1,13 @@
 // Repositories Interfaces sets the definition (not the implementation) on persisting data
 
+import { PaginationParams } from '@/core/repositories/pagination-params'
 import { QuestionComment } from '../../enterprise/entities/question-comment'
 
 export interface QuestionCommentsRepository {
+  findManyByQuestionId(
+    questionId: string,
+    params: PaginationParams,
+  ): Promise<QuestionComment[]>
   findById(id: string): Promise<QuestionComment | null>
   create(questionComment: QuestionComment): Promise<void>
   delete(questionComment: QuestionComment): Promise<void>
--- src/domain/forum/application/use-cases/delete-question-comment.ts
+++ src/domain/forum/application/use-cases/delete-question-comment.ts
@@ -1,6 +1,3 @@
-import { UniqueEntityID } from '@/core/entities/unique-entity-id'
-import { QuestionsRepository } from '../repositories/questions-repository'
-import { QuestionComment } from '../../enterprise/entities/question-comment'
 import { QuestionCommentsRepository } from '../repositories/question-comments-repository'
 
 interface DeleteQuestionCommentUseCaseRequest {
